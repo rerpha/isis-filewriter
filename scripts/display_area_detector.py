f1ff5e4278b3c0986f6a29dfce190af051606236
Dominic Oram <dominic.oram@stfc.ac.uk>
2020-10-22 15:25:33 +0100

    Improve image decompression script

--- scripts/display_area_detector.py
+++ scripts/display_area_detector.py
@@ -1,30 +1,44 @@
 from kafka import KafkaConsumer, TopicPartition
 from streaming_data_types import area_detector_NDAr
-from matplotlib import colors
+from streaming_data_types.fbschemas.NDAr_NDArray_schema.DType import DType
 import matplotlib.pyplot as plt
+from struct import *
+
+NUM_OF_MESSAGES = 2
+
+types = {
+    DType.Int32: (4, "i"),
+    DType.Int64: (8, "q"),
+    DType.Float32: (4, "f"),
+    DType.Float64: (8, "d"),
+}
 
 consumer = KafkaConsumer(bootstrap_servers='livedata.isis.cclrc.ac.uk:9092', auto_offset_reset='earliest')
-topic_partition = TopicPartition('DEMO_areaDetector', 0)
+topic_partition = TopicPartition('LOQ_areaDetector', 0)
 consumer.assign([topic_partition])
 end = consumer.end_offsets([topic_partition])
 
-consumer.seek(topic_partition, end[topic_partition]-1)
+consumer.seek(topic_partition, end[topic_partition]-NUM_OF_MESSAGES)
 
-message = next(consumer)
-ad_message = area_detector_NDAr.deserialise_ndar(message.value)
+for message in range(NUM_OF_MESSAGES):
+    message = next(consumer)
+    ad_message = area_detector_NDAr.deserialise_ndar(message.value)
 
-image = []
-index = 0
-for y in range(ad_message.dims[0]):
-    row = []
-    for x in range(ad_message.dims[1]):
-        first_byte = ad_message.data[index:index + 8]
+    data_type = types[ad_message.data_type]
 
-        total = sum([num * pow(2, i * 8) for i, num in enumerate(first_byte)])
+    image = []
+    index = 0
+    for y in range(ad_message.dims[0]):
+        row = []
+        for x in range(ad_message.dims[1]):
+            data_point = ad_message.data[index:index + data_type[0]]
 
-        row.append(total)
-        index += 8
-    image.append(row)
+            byte_data = pack('B' * data_type[0], *data_point)
+            converted_value = unpack(data_type[1], byte_data)[0]
 
-plt.imshow(image)
-plt.show()
+            row.append(converted_value)
+            index += data_type[0]
+        image.append(row)
+
+    plt.imshow(image)
+    plt.show()
